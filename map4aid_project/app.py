e5ec074f9eacaedfa6b9174f716d0a24be83e1d1
Nicola <nicolaluciano01@gmail.com>
2026-01-23 19:41:36 +0100

    Refactor modelli, fix relazioni, reset migrations, update controller autenticazione con prova richiesta postman

--- map4aid_project/app.py
+++ map4aid_project/app.py
@@ -3,26 +3,22 @@ from flask import Flask
 from config import db, migrate
 
 def create_app():
-    # Attiva la gestione della cartella instance/
     app = Flask(__name__, instance_relative_config=True)
 
-    #mi assicuro che esista la cartella instance
     os.makedirs(app.instance_path, exist_ok=True)
 
-    #configurazione db, sempre dentro instance
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{app.instance_path}/database.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'skMomentanea'
 
-    # Inizializza estensioni
     db.init_app(app)
     migrate.init_app(app, db)
 
     import models
 
-    import AutenticazioneControl
-    AutenticazioneControl.init_routes(app)
-    import EmailControl
+    from controllers.autenticazione_control import auth_bp
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
 
     @app.route("/")
     def home():
